fe63965a43268836c3257313310da04e120aade4
vmahiyoga <119960618+vmahiyoga@users.noreply.github.com>
2022-12-17 23:28:55 +0530

    Add files via upload

---
+++ week3/day1/Assignment1/system/Desktop.java
@@ -0,0 +1,10 @@
+package org.system;
+
+public class Desktop extends Computer{
+	
+	public int desktopSize()
+	{
+		return 13;
+	}
+
+}
---
+++ week3/day1/Assignment1/system/MySystemDetails.java
@@ -0,0 +1,13 @@
+package org.system;
+
+public class MySystemDetails {
+
+	public static void main(String[] args) {
+		//Create object for the Desktop class
+		Desktop details = new Desktop();
+		
+		details.computerModel();
+		System.out.println(details.desktopSize());
+	}
+
+}
